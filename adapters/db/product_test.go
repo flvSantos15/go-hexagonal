aaee197ceb4715ea0027d0505e39fd31861cfb75
flvsantos15 <flvsantos300@gmail.com>
2024-07-25 08:55:26 -0300

    feat: add save method and save test

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,8 +1,13 @@
-package db
+package db_test
 
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/flvsantos15/go-hexagonal/adapters/db"
+	"github.com/flvsantos15/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -34,4 +39,38 @@ func createProduct(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductGet(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDbSave(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
